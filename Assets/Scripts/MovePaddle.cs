1db54a75a3dad019b881f3af7916f6d9708e1506
Suchaaver Chahal <suchaaverchahal@gmail.com>
2021-05-18 08:52:15 -0700

    basic paddle movement working

---
+++ Assets/Scripts/MovePaddle.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddle : MonoBehaviour
+{
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectInput();
+    }
+
+    void DetectInput() {
+        float dir = Input.GetAxis("Vertical");
+        if(dir < 0){
+            transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+        } else if (dir > 0){
+            transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+        }
+    }
+}
